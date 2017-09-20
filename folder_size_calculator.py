66f26d129da946aa5520b08191935b5ade141a89
Felipe Dias <philixo@gmail.com>
2017-09-19 23:19:20 -0500

    Create folder_size_calculator.py

---
+++ folder_size_calculator.py
@@ -0,0 +1,48 @@
+import dropbox
+import numpy as np
+import pandas as pd
+
+access_token = 'INSERT_YOUR_TOKEN_HERE'
+
+dbx = dropbox.Dropbox(access_token)
+
+all_folders = []
+all_hierarchies = []
+all_sizes_cumul = []
+
+def main():
+    check_folders('', 1)
+    folder_db = pd.DataFrame({"folder":all_folders})
+    folder_db["hierarchy"] = all_hierarchies
+    folder_db["size_cumul"] = all_sizes_cumul
+    for i,this_size in enumerate(["kb","mb","gb"]):
+        folder_db["size_cumul_" + this_size] = folder_db["size_cumul"] * 1.0 / (1024 ** (i+1))
+    folder_db.to_csv("folder_sizes.csv",index=False, encoding='utf8')
+
+def check_folders(folder_path, hierarchy):
+    folder_temp_list = []
+    file_temp_list = []
+    this_folder_size = 0.0
+    for entry in dbx.files_list_folder(folder_path).entries:
+        if type(entry) == dropbox.files.FolderMetadata:
+            folder_temp_list.append(entry)
+        if type(entry) == dropbox.files.FileMetadata:
+            file_temp_list.append(entry)
+    for this_subfolder in folder_temp_list:
+        this_subfolder_name = this_subfolder.name
+        this_subfolder_full_path = folder_path + "/" + this_subfolder_name
+        this_subfolder_size = check_folders(this_subfolder_full_path,hierarchy+1)
+        this_folder_size = this_folder_size + this_subfolder_size
+    for this_file in file_temp_list:
+            this_folder_size = this_folder_size + this_file.size
+    if folder_path == '':
+        all_folders.append('/')
+    else:
+        all_folders.append(folder_path)
+    all_sizes_cumul.append(this_folder_size)
+    all_hierarchies.append(hierarchy)
+    if hierarchy != 1:
+        return this_folder_size
+
+if __name__ == '__main__':
+    main()
